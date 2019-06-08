fa8696e4f5b9e03b6ec16c9344d700f876fadb71
Josh Landron <JoshLandron@gmail.com>
2019-06-07 22:10:39 -0700

    a few updates to FileServer

--- FileServer.java
+++ FileServer.java
@@ -2,6 +2,7 @@ import java.rmi.*;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.Vector;
 import java.io.*;
+import java.nio.file.Files;
 
 class FileServer extends UnicastRemoteObject implements ServerInterface {
     private String port;
@@ -18,14 +19,38 @@ class FileServer extends UnicastRemoteObject implements ServerInterface {
     }
 
     public FileContents download(String client, String filename, String mode) throws RemoteException {
-        File file = null;
+        FileContents fileContents;
         for (int i = 0; i < this.files.size(); i++) {
-
+            if (files.elementAt(i).getName() == filename) {
+                try {
+                    fileContents = new FileContents(Files.readAllBytes(files.elementAt(i).toPath()));
+                    return fileContents;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
         }
-        return file.download(client, mode);
+        File file = new File("files/" + filename);
+
+        return null;
     }
 
     public boolean upload(String client, String filename, FileContents contents) throws RemoteException {
+        // check if file exists, if it does, write over file assuming only one client
+        // can change a file at a time. change later to check client state??
+        for (int i = 0; i < this.files.size(); i++) {
+            if (files.elementAt(i).getName() == filename) {
+                try {
+                    FileOutputStream foStream = new FileOutputStream("files/" + filename);
+                    foStream.write(contents.get());
+                    foStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return false;
+                }
+                return true;
+            }
+        }
         return false;
     }
 
