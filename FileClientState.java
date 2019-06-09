0ce692c5c7e7665388be92282f59633edde0d680
Ed Abshire <thaldin@comcast.net>
2019-06-08 21:50:36 -0700

    FileClientState - added comments, removed unneeded include.

--- FileClientState.java
+++ FileClientState.java
@@ -1,5 +1,6 @@
-import java.io.File;
-
+/**
+ * FileClientState enumeration to determine the state of the client
+ */
 public enum FileClientState
 {
     INVALID("I"),
@@ -7,17 +8,34 @@ public enum FileClientState
     WRITE_OWNED("W"),
     RELEASE_OWNERSHIP("X");
 
+    // Local variable to hold the id of the enum
     private String id;
 
+    /**
+     * "Constructor" - allows the enum to have a value
+     *
+     * @param id The single character value representing the enum
+     */
     FileClientState(String id)
     {
         this.id = id;
     }
 
+    /**
+     * Returns the string value of the enum
+     *
+     * @return the enum id ("R", "W", "I", "X")
+     */
     public String getId() {
         return this.id;
     }
 
+    /***
+     * Gets the enumeration value based on the string id passed in
+     *
+     * @param id string value of the enum to get.
+     * @return FileClientState based on the enum, null otherwise
+     */
     public static FileClientState fromId(String id)
     {
         for (FileClientState state : FileClientState.values())
