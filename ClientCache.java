4eb653be212006257356477cb3529bb0be1e62d3
Ed Abshire <thaldin@comcast.net>
2019-06-10 19:42:17 -0700

    Documented ClientCache cleaned up warnings

--- ClientCache.java
+++ ClientCache.java
@@ -3,13 +3,20 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class ClientCache {
+/**
+ * ClientCache class stores the file locally for the client in order
+ * to improve performance if the same file is used over and over.
+ */
+class ClientCache {
 
-    final String tempDir = System.getProperty("java.io.tmpdir");
-    final Path _cacheFilePath = Paths.get(tempDir + "/abshirelandron.txt");
+    private final String tempDir = System.getProperty("java.io.tmpdir");
+    private final Path _cacheFilePath = Paths.get(tempDir + "/abshirelandron.txt");
     private String _fileName = "";
     private ClientState _state = ClientState.INVALID;
 
+    /**
+     * Constructor - deletes an existing cache on creation.
+     */
     ClientCache() {
         if (Files.exists(_cacheFilePath)) {
             try {
@@ -20,16 +27,18 @@ public class ClientCache {
         }
     }
 
-    public boolean clearCache() {
+    /**
+     * Clears out the cache
+     *
+     */
+    void clearCache() {
         try {
             if (Files.exists(_cacheFilePath)) {
                 UnixTools.changeFileMode(_cacheFilePath.toString(), true);
                 Files.delete(_cacheFilePath);
             }
-            return true;
         } catch (Exception ex) {
             System.err.println("Error clearing cache: " + ex.getMessage());
-            return false;
         }
     }
 
@@ -83,27 +92,26 @@ public class ClientCache {
     }
 
     /**
+     * Creates a cache from a new FileContent object in the temp folder for the user
      *
-     * @param contents
-     * @return
+     * @param contents FileContent object containing the requested file
      */
-    boolean createCache(FileContents contents) {
+    void createCache(FileContents contents) {
         try {
             Files.write(_cacheFilePath, contents.get());
         } catch (IOException ex) {
             System.err.println("Error creating cache: " + ex.getMessage());
-            return false;
         }
 
-        return true;
     }
 
     /**
+     * Returns the content of the cache in a FileContents object
      *
-     * @return
+     * @return FileContent object containing the file, null on error
      */
     FileContents getCache() {
-        FileContents contents = null;
+        FileContents contents;
         try {
             contents = new FileContents(Files.readAllBytes(_cacheFilePath));
         } catch (IOException ex) {
@@ -115,9 +123,11 @@ public class ClientCache {
     }
 
     /**
+     * Checks to see if the file name passed in is contained in the cache
      *
-     * @param fileName
-     * @return
+     * @param fileName String containing the name of the file to check to
+     *                 see if it is in the cache
+     * @return True if the cache contains the file, false otherwise
      */
     boolean cacheContainsFile(String fileName) {
         return _fileName.equals(fileName);
