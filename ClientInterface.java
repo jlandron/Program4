d8c96edd7e68857c61a9bf80c064d8f23b89f12e
Ed Abshire <thaldin@comcast.net>
2019-06-10 19:45:23 -0700

    Comments

--- ClientInterface.java
+++ ClientInterface.java
@@ -1,5 +1,8 @@
 import java.rmi.*;
 
+/**
+ * RMI Required interface for the FileClient
+ */
 public interface ClientInterface extends Remote {
     public boolean invalidate( ) throws RemoteException;
     public boolean writeback( ) throws 	RemoteException;
